a5b9f5f75e9e748608ef57a1bd35a19983e5eb97
Nigel Stewart <nigels@nigels.com>
2017-11-04 16:11:58 +1000

    ffplay.sh wrapper script

---
+++ bin/ffplay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# Wrapper script for playing videos with ffplay
+# - Default X11 DISPLAY to :0
+# - Buffer 10MB of input
+# - Force fullscreen 1920x1080
+# - Exit once finished
+#
+
+DISPLAY=:0 stdbuf -o 10MB ffplay -fs -x 1920 -y 1080 -autoexit -noborder - < $*
+
